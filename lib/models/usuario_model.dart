aee750e9c6cdf3e198e8fd6dcefd366799c0af5c
Iago de Souza Lopes <iagodesouza2012@gmail.com>
2024-04-22 20:09:52 -0300

    Criação da classe usuário

---
+++ lib/models/usuario_model.dart
@@ -0,0 +1,11 @@
+class Usuario {
+  final String uid;
+  final String nivel;
+  final String empresa;
+
+  Usuario({
+    required this.uid,
+    required this.nivel,
+    required this.empresa,
+  });
+}
