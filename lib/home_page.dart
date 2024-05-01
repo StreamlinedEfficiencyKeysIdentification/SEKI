3fa729b100acbeacaa837c906626aa662667276c
Iago de Souza Lopes <iagodesouza2012@gmail.com>
2024-04-30 23:57:51 -0300

    Alterando a home com permissões de acesso e ajustes de layout

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,35 +1,13 @@
-// ignore_for_file: avoid_print, use_build_context_synchronously
+// ignore_for_file: use_build_context_synchronously, avoid_print
 
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import '../controllers/usuario_controller.dart';
+import 'models/usuario_model.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
-  Future<String?> _getUserName() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    String? usuarioLogado = prefs.getString('usuarioLogado');
-
-    if (usuarioLogado != null) {
-      DocumentSnapshot<Map<String, dynamic>> userDoc = await FirebaseFirestore
-          .instance
-          .collection('Usuarios')
-          .doc(usuarioLogado)
-          .get();
-
-      if (userDoc.exists) {
-        String? userName = userDoc.data()?['Nome'];
-        return userName;
-      } else {
-        print('Documento do usuário não encontrado no Firestore.');
-        return null;
-      }
-    } else {
-      return null;
-    }
-  }
 
   Future<void> _logout(BuildContext context) async {
     try {
@@ -60,49 +38,78 @@ class HomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            FutureBuilder<String?>(
-              future: _getUserName(),
+            FutureBuilder<Usuario>(
+              future: UsuarioController.getUsuarioLogado(),
               builder: (context, snapshot) {
                 if (snapshot.connectionState == ConnectionState.waiting) {
                   return const CircularProgressIndicator();
                 } else if (snapshot.hasError) {
                   return const Text('Erro ao carregar o nome do usuário.');
                 } else {
-                  String? userName = snapshot.data;
-                  return Text(
-                    userName != null
-                        ? 'Bem-vindo,\n$userName!'
-                        : 'Usuário desconhecido.',
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(fontSize: 24),
+                  Usuario usuario = snapshot.data ??
+                      Usuario(uid: '', nivel: '', empresa: '', nome: '');
+
+                  int nivelUsuario = int.tryParse(usuario.nivel) ?? 0;
+                  String nome = usuario.nome;
+
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Bem-vindo,\n$nome!',
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(fontSize: 24),
+                      ),
+                      const SizedBox(height: 20),
+                      Visibility(
+                        visible: nivelUsuario <= 4,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/scan');
+                          },
+                          child: const Text('Ler QR Code'),
+                        ),
+                      ),
+                      Visibility(
+                        visible: nivelUsuario <= 2,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/setor');
+                          },
+                          child: const Text('Registrar Novo Setor'),
+                        ),
+                      ),
+                      Visibility(
+                        visible: nivelUsuario <= 2,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/group');
+                          },
+                          child: const Text('Registrar Nova Empresa'),
+                        ),
+                      ),
+                      Visibility(
+                        visible: nivelUsuario <= 3,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/register');
+                          },
+                          child: const Text('Registrar Novo Usuário'),
+                        ),
+                      ),
+                      Visibility(
+                        visible: nivelUsuario <= 3,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/hardware');
+                          },
+                          child: const Text('Registrar Novo Equipamento'),
+                        ),
+                      ),
+                    ],
                   );
                 }
               },
-            ),
-            const SizedBox(height: 20), // Espaçamento entre o texto e o botão
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/register');
-              },
-              child: const Text('Registrar Novo Usuário'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/group');
-              },
-              child: const Text('Registrar Nova Empresa'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/setor');
-              },
-              child: const Text('Registrar Novo Setor'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/hardware');
-              },
-              child: const Text('Registrar Novo Equipamento'),
             ),
           ],
         ),
