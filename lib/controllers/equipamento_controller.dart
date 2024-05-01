485c4412a7d5a10d632fdbf88be526716083cf40
Iago de Souza Lopes <iagodesouza2012@gmail.com>
2024-04-30 23:57:29 -0300

    Implementando o qr code no equipamento

--- lib/controllers/equipamento_controller.dart
+++ lib/controllers/equipamento_controller.dart
@@ -1,9 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 import '../controllers/usuario_controller.dart';
 import '../models/usuario_model.dart';
 
 class EquipamentoController {
-  static Future<void> cadastrarEquipamento(
+  static Future<bool> cadastrarEquipamento(
+    BuildContext context,
     String qrcode,
     String empresaId,
     String setorId,
@@ -12,39 +14,64 @@ class EquipamentoController {
     String marca,
     String modelo,
   ) async {
-    String statusS = '';
-    // Obter o próximo ID disponível na coleção 'Equipamento'
-    int proximoId = await _getProximoIdEquipamento();
+    try {
+      String statusS = '';
+      // Obter o próximo ID disponível na coleção 'Equipamento'
+      int proximoId = await _getProximoIdEquipamento();
 
-    Usuario usuario = await UsuarioController.getUsuarioLogado();
+      Usuario usuario = await UsuarioController.getUsuarioLogado();
 
-    if (status) {
-      statusS = 'Ativo';
-    } else {
-      statusS = 'Inativo';
+      String matriz = '';
+      DocumentSnapshot empresaSnapshot = await FirebaseFirestore.instance
+          .collection('Empresa')
+          .doc(usuario.empresa)
+          .get();
+
+      matriz = empresaSnapshot['EmpresaPai'].toString();
+
+      if (status) {
+        statusS = 'Ativo';
+      } else {
+        statusS = 'Inativo';
+      }
+
+      // Inserir os detalhes do equipamento na coleção 'Equipamento'
+      await FirebaseFirestore.instance
+          .collection('Equipamento')
+          .doc(proximoId.toString())
+          .set({
+        'IDqrcode': qrcode,
+        'IDempresa': empresaId,
+        'IDsetor': setorId,
+        'IDusuario': usuarioId,
+        'Status': statusS,
+        'QuemCriou': usuario.uid,
+      });
+
+      // Inserir os detalhes adicionais (marca e modelo) na coleção 'DetalheEquipamento'
+      await FirebaseFirestore.instance
+          .collection('DetalheEquipamento')
+          .doc(proximoId.toString())
+          .set({
+        'Marca': marca,
+        'Modelo': modelo,
+      });
+
+      await FirebaseFirestore.instance
+          .collection('QRcode')
+          .doc(
+              matriz) // Usar o ID da empresa como ID do documento na coleção QRcode
+          .set({
+        qrcode: {
+          'QuemCriou': usuario.uid,
+          'DataCriacao': FieldValue.serverTimestamp(),
+        },
+      }, SetOptions(merge: true));
+
+      return true;
+    } catch (e) {
+      return false;
     }
-
-    // Inserir os detalhes do equipamento na coleção 'Equipamento'
-    await FirebaseFirestore.instance
-        .collection('Equipamento')
-        .doc(proximoId.toString())
-        .set({
-      'IDqrcode': qrcode,
-      'IDempresa': empresaId,
-      'IDsetor': setorId,
-      'IDusuario': usuarioId,
-      'Status': statusS,
-      'QuemCriou': usuario.uid,
-    });
-
-    // Inserir os detalhes adicionais (marca e modelo) na coleção 'DetalheEquipamento'
-    await FirebaseFirestore.instance
-        .collection('DetalheEquipamento')
-        .doc(proximoId.toString())
-        .set({
-      'Marca': marca,
-      'Modelo': modelo,
-    });
   }
 
   static Future<int> _getProximoIdEquipamento() async {
