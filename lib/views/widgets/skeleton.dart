87145618cf96f4e99f7c01d2d4532a9e6eb36c06
Iago de Souza Lopes <iagodesouza2012@gmail.com>
2024-05-20 22:16:54 -0300

    Novo skeleton e inserindo o antigo no mesmo arquivo

--- lib/views/widgets/skeleton.dart
+++ lib/views/widgets/skeleton.dart
@@ -1,5 +1,37 @@
-// lib/widgets/skeleton_item.dart
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+class SkeletonLoader extends StatelessWidget {
+  const SkeletonLoader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: 5, // Quantidade de itens esqueléticos a serem exibidos
+              itemBuilder: (context, index) => Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Shimmer.fromColors(
+                  baseColor: Colors.grey[300]!,
+                  highlightColor: Colors.grey[100]!,
+                  child: Container(
+                    height: 60,
+                    color: Colors.grey[300],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
 class SkeletonItem extends StatelessWidget {
   const SkeletonItem({super.key});
