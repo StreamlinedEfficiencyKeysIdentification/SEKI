7f5e86a857c00699794ada6d4d847ed0c80b941d
Iago de Souza Lopes <iagodesouza2012@gmail.com>
2024-06-07 17:11:06 -0300

    Verificação de erros, validação de campos, ajustes de layout e inserção de dados no banco de dados(Chamados).

--- lib/views/screens/chamado/chamado.dart
+++ lib/views/screens/chamado/chamado.dart
@@ -1,8 +1,9 @@
-// ignore_for_file: avoid_print
+// ignore_for_file: avoid_print, use_build_context_synchronously
 
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:testeseki/controllers/usuario_controller.dart';
+import 'package:testeseki/models/equipamento_model.dart';
 import 'package:testeseki/models/usuario_model.dart';
 import 'package:testeseki/views/screens/barcode/scan_code.dart';
 
@@ -21,13 +22,16 @@ class Chamado extends StatefulWidget {
 }
 
 class ChamadoState extends State<Chamado> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController _equipamentoController = TextEditingController();
   final GlobalKey<AutocompleteUsuarioExampleState> _autocompleteKey =
       GlobalKey();
+  final TextEditingController _tituloController = TextEditingController();
+  final TextEditingController _descricaoController = TextEditingController();
   bool _isValid = false;
   bool waiting = false;
   String _usuario = '';
-  Usuario usuario = Usuario(
+  Usuario user = Usuario(
     uid: '',
     nivel: '',
     empresa: '',
@@ -40,6 +44,17 @@ class ChamadoState extends State<Chamado> {
     dataAcesso: '',
     primeiroAcesso: false,
     redefinirSenha: false,
+  );
+  Equipamento equip = Equipamento(
+    id: '',
+    marca: '',
+    modelo: '',
+    qrcode: '',
+    empresa: '',
+    setor: '',
+    usuario: '',
+    criador: '',
+    status: '',
   );
   String _usuarioSelecionado = '';
 
@@ -62,15 +77,20 @@ class ChamadoState extends State<Chamado> {
   }
 
   Future<void> _carregarUsuarioLogado() async {
-    // Obtém as informações do usuário logado
-    Usuario usuario = await UsuarioController.getUsuarioLogado();
-
-    // Atualiza os valores das variáveis _usuario e _usuarioSelecionado
-    setState(() {
-      _usuario = usuario.nome;
-      _usuarioSelecionado = usuario.uid;
-      waiting = true;
-    });
+    try {
+      waiting = false;
+      Usuario usuario = await UsuarioController.getUsuarioLogado();
+      setState(() {
+        user = usuario;
+        _usuario = usuario.nome;
+        _usuarioSelecionado = usuario.uid;
+        waiting = true;
+      });
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Erro ao carregar usuário logado.')),
+      );
+    }
   }
 
   void _onTextChanged() {
@@ -84,14 +104,56 @@ class ChamadoState extends State<Chamado> {
   }
 
   Future<void> _searchFirestore(String code) async {
-    final snapshot = await FirebaseFirestore.instance
-        .collection('Equipamento')
-        .where('IDqrcode', isEqualTo: code)
-        .get();
+    try {
+      final equipamentoSnapshot = await FirebaseFirestore.instance
+          .collection('Equipamento')
+          .where('IDqrcode', isEqualTo: code)
+          .get();
 
-    setState(() {
-      _isValid = snapshot.docs.isNotEmpty;
-    });
+      if (equipamentoSnapshot.docs.isNotEmpty) {
+        final equipamentoDoc = equipamentoSnapshot.docs.first;
+        final equipamentoData = equipamentoDoc.data();
+
+        // Usar o ID do documento do equipamento para buscar detalhes na coleção DetalheEquipamento
+        final detalheEquipamentoSnapshot = await FirebaseFirestore.instance
+            .collection('DetalheEquipamento')
+            .doc(equipamentoDoc.id)
+            .get();
+
+        if (detalheEquipamentoSnapshot.exists) {
+          final detalheEquipamentoData =
+              detalheEquipamentoSnapshot.data() as Map<String, dynamic>;
+
+          setState(() {
+            _isValid = true;
+            equip = Equipamento(
+              id: equipamentoDoc.id,
+              marca: detalheEquipamentoData['Maraa'] ?? '',
+              modelo: detalheEquipamentoData['Modelo'] ?? '',
+              qrcode: equipamentoData['IDqrcode'] ?? '',
+              empresa: equipamentoData['IDempresa'] ?? '',
+              setor: equipamentoData['IDsetor'] ?? '',
+              usuario: equipamentoData['IDusuario'] ?? '',
+              criador: equipamentoData['QuemCriou'] ?? '',
+              status: equipamentoData['Status'] ?? '',
+            );
+          });
+        } else {
+          setState(() {
+            _isValid = false;
+          });
+        }
+      } else {
+        setState(() {
+          _isValid = false;
+        });
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+            content: Text('Erro ao buscar equipamento no Firestore.')),
+      );
+    }
   }
 
   Future<void> _openScanner() async {
@@ -111,6 +173,91 @@ class ChamadoState extends State<Chamado> {
     }
   }
 
+  Future<void> _submitForm() async {
+    if (_formKey.currentState?.validate() ?? false) {
+      int nivelUsuario = int.tryParse(user.nivel) ?? 0;
+      if (!_isValid) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Equipamento não existe.')),
+        );
+        return;
+      } else if (user.empresa != equip.empresa && nivelUsuario == 4) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+              content: Text('Você não tem permissão para essa empresa.')),
+        );
+        return;
+      } else if (_usuarioSelecionado.isEmpty) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Selecione um usuário')),
+        );
+      } else {
+        // Lógica para enviar o formulário
+        try {
+          // Referência para a coleção "Chamados"
+          final chamadosRef = FirebaseFirestore.instance.collection('Chamados');
+
+          // Buscar o ID da EmpresaPai na coleção "Empresa"
+          final empresaDoc = await FirebaseFirestore.instance
+              .collection('Empresa')
+              .doc(user.empresa)
+              .get();
+          final empresaData = empresaDoc.data();
+          final empresaPaiId = empresaData?['EmpresaPai'];
+
+          // Buscar o último ID de chamado na coleção "Chamados"
+          int ultimoChamado = 0;
+          final ultimoChamadoDoc = await chamadosRef.doc(empresaPaiId).get();
+          if (ultimoChamadoDoc.exists) {
+            final chamadosData = ultimoChamadoDoc.data();
+            chamadosData?.forEach((key, value) {
+              if (int.tryParse(key) != null) {
+                final chamadoId = int.parse(key);
+                if (chamadoId > ultimoChamado) {
+                  ultimoChamado = chamadoId;
+                }
+              }
+            });
+          }
+
+          // Incrementar o ID do chamado
+          final novoChamadoId = (ultimoChamado + 1).toString().padLeft(6, '0');
+
+          // Dados do chamado
+          final chamadoData = {
+            'QRcode': equip.qrcode,
+            'titulo': _tituloController.text,
+            'usuario': _usuarioSelecionado,
+            'descricao': _descricaoController.text,
+            'empresa': equip.empresa,
+          };
+
+          // Adicionar os dados do chamado ao documento na coleção "Chamados"
+          await chamadosRef
+              .doc(empresaPaiId)
+              .set({novoChamadoId: chamadoData}, SetOptions(merge: true));
+
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Chamado enviado com sucesso')),
+          );
+
+          // Limpar o formulário após envio
+          _formKey.currentState?.reset();
+          setState(() {
+            _equipamentoController.clear();
+            _carregarUsuarioLogado();
+            _isValid = false;
+          });
+        } catch (e) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+                content: Text('Erro ao enviar chamado para o Firestore.')),
+          );
+        }
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -124,135 +271,151 @@ class ChamadoState extends State<Chamado> {
         ),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-              child: Column(
-                children: [
-                  TextField(
-                    keyboardType: TextInputType.number,
-                    controller: _equipamentoController,
-                    decoration: InputDecoration(
-                      prefixIcon: const Icon(
-                        Icons.laptop,
-                        color: Colors.blue,
-                      ),
-                      hintText: "Insira um equipamento",
-                      focusedBorder: const UnderlineInputBorder(
-                        borderSide: BorderSide(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      keyboardType: TextInputType.number,
+                      controller: _equipamentoController,
+                      decoration: InputDecoration(
+                        prefixIcon: const Icon(
+                          Icons.laptop,
                           color: Colors.blue,
-                          width: 5,
                         ),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide: BorderSide(
-                          color: _isValid ? Colors.green : Colors.red,
-                          width: 2,
-                          style: BorderStyle.solid,
-                        ),
-                      ),
-                      suffixIcon: Container(
-                        margin: const EdgeInsets.all(8),
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            minimumSize: const Size(100, 50),
-                            backgroundColor: Colors.blue,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30.0),
-                            ),
-                          ),
-                          onPressed: _openScanner,
-                          child: const Text(
-                            "Search",
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.white,
-                            ),
+                        hintText: "Insira um equipamento",
+                        focusedBorder: const UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.blue,
+                            width: 5,
                           ),
                         ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 16),
-                  TextField(
-                    decoration: InputDecoration(
-                      prefixIcon: const Icon(
-                        Icons.person,
-                        color: Colors.blue,
-                      ),
-                      hintText: "Insira um Titulo",
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide: const BorderSide(
-                          color: Colors.black,
-                          width: 1,
-                          style: BorderStyle.solid,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide(
+                            color: _isValid ? Colors.green : Colors.red,
+                            width: 2,
+                            style: BorderStyle.solid,
+                          ),
+                        ),
+                        suffixIcon: Container(
+                          margin: const EdgeInsets.all(8),
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              minimumSize: const Size(100, 50),
+                              backgroundColor: Colors.blue,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30.0),
+                              ),
+                            ),
+                            onPressed: _openScanner,
+                            child: const Text(
+                              "Search",
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
                         ),
                       ),
-                    ),
-                  ),
-                  const SizedBox(height: 16),
-                  if (waiting)
-                    AutocompleteUsuarioExample(
-                      user: _usuario,
-                      key: _autocompleteKey,
-                      onUsuarioSelected: (usuario) {
-                        setState(() {
-                          _usuarioSelecionado = usuario;
-                        });
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Campo obrigatório';
+                        }
+                        return null;
                       },
                     ),
-                  const SizedBox(height: 16),
-                  TextField(
-                    decoration: InputDecoration(
-                      prefixIcon: const Icon(
-                        Icons.person,
-                        color: Colors.blue,
+                    const SizedBox(height: 16),
+                    TextFormField(
+                      controller: _tituloController,
+                      decoration: InputDecoration(
+                        prefixIcon: const Icon(
+                          Icons.person,
+                          color: Colors.blue,
+                        ),
+                        hintText: "Insira um Titulo",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: const BorderSide(
+                            color: Colors.black,
+                            width: 1,
+                            style: BorderStyle.solid,
+                          ),
+                        ),
                       ),
-                      hintText: "Descrição: ",
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide: const BorderSide(
-                          color: Colors.black,
-                          width: 1,
-                          style: BorderStyle.solid,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Campo obrigatório';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 16),
+                    if (waiting)
+                      AutocompleteUsuarioExample(
+                        user: _usuario,
+                        key: _autocompleteKey,
+                        onUsuarioSelected: (usuario) {
+                          setState(() {
+                            _usuarioSelecionado = usuario;
+                          });
+                        },
+                      ),
+                    const SizedBox(height: 16),
+                    TextFormField(
+                      controller: _descricaoController,
+                      decoration: InputDecoration(
+                        prefixIcon: const Icon(
+                          Icons.description,
+                          color: Colors.blue,
+                        ),
+                        hintText: "Descrição: ",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: const BorderSide(
+                            color: Colors.black,
+                            width: 1,
+                            style: BorderStyle.solid,
+                          ),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Campo obrigatório';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 16),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: const Size(100, 50),
+                        backgroundColor: Colors.blue,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                      ),
+                      onPressed: _submitForm,
+                      child: const Text(
+                        "Enviar",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
                         ),
                       ),
                     ),
-                  ),
-                  const SizedBox(height: 16),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: const Size(100, 50),
-                      backgroundColor: Colors.blue,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30.0),
-                      ),
-                    ),
-                    child: const Text(
-                      "Enviar",
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.white,
-                      ),
-                    ),
-                    onPressed: () {
-                      print(_usuarioSelecionado);
-                      if (_usuarioSelecionado.isEmpty) {
-                        const SnackBar(
-                          content: Text('Selecione um usuário'),
-                          duration: Duration(seconds: 2),
-                        );
-                      }
-                    },
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
