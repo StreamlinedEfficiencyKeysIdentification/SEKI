cc4a07c4a9e37bb65d3ad78e79b9479c69cde072
Iago de Souza Lopes <iagodesouza2012@gmail.com>
2024-06-12 16:56:33 -0300

    Model de chamado

---
+++ lib/models/chamado_model.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: non_constant_identifier_names
+
+class Chamado {
+  final String IDdoc;
+  final String IDchamado;
+  final String QRcode;
+  final String Titulo;
+  final String Usuario;
+  final String Descricao;
+  final String Empresa;
+  final String Status;
+  final String Responsavel;
+  final String EmpresaResponsavel;
+  final String DataCriacao;
+  final String DataAtualizacao;
+  final bool Lido;
+
+  Chamado({
+    required this.IDdoc,
+    required this.IDchamado,
+    required this.QRcode,
+    required this.Titulo,
+    required this.Usuario,
+    required this.Descricao,
+    required this.Empresa,
+    required this.Status,
+    required this.Responsavel,
+    required this.EmpresaResponsavel,
+    required this.DataCriacao,
+    required this.DataAtualizacao,
+    required this.Lido,
+  });
+}
